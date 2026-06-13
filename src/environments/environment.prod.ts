78d5eb0a1a98bf5179b9fb6c668bf5fc5dadee9a
yusrilmahendri <yusrilmahendri.yusril@gmail.com>
2026-06-13 14:51:11 +0700

    style: env proud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  apiBaseUrl: 'https://horuzt.pioneersolve.id/api'
+  apiBaseUrl: 'https://cloud-api.sena-digital.com/api'
 };
